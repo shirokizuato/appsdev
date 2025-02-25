5fb7cad778f824e6f1ecffbc124deb0146bade29
shirokizuato <danenacs@gmail.com>
2025-02-25 13:31:31 +0800

    Add files via upload

---
+++ Problem 1.py
@@ -0,0 +1,9 @@
+print('Dane Ralph Nacario')
+print('BSIT-2A')
+print ('APPSDEV')
+print ('Feb. 25, 2025')
+
+
+str = input ("Enter a string:")
+new_str = str.upper()
+print ('Uppercase String: {new_str})
